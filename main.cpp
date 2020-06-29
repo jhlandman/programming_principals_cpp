730183567afaa10c99b1710a26ecee8f253cdc8f
Jayson Landman <jaysonhenrylandman@yahoo.com>
2020-06-29 20:55:31 +0000

    up to ch 4.4

--- main.cpp
+++ main.cpp
@@ -33,4 +33,36 @@ int main() {
   int x = 100;
   char y = x;
   std::cout << y << "\n";
+
+  // narrowers
+  /*
+  double x2 {2.7};
+  int y2 {x2}; */
+  // implicit conversion will throw an error
+
+  // constant expressions
+  constexpr double pi = 3.14159265359;
+  double circum = 2 * pi;
+  std::cout << circum << "\n";
+
+  // switch statements
+  /*
+  constexpr double cm_per_sq_inch = 2.54;
+  double length = 1;
+  char unit = 'a';
+  std::cout << "please enter a length followed by a unit (c or i) : \n";
+  std::cin >> length >> unit;
+  switch (unit) { // you cannot switch on a string
+    case 'i':
+      std::cout << length << " in == " << cm_per_sq_inch * length << "cm\n";
+      break;
+    case 'c':
+      std::cout << length << "cm == " << length / cm_per_sq_inch << "in\n";
+      break;
+    default:
+      std::cout << "invalid unit \n";
+      break;
+  } */
+
+  //
 } 
